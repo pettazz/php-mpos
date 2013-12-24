f38d8129189a7a488dc743c49b71a2c232844775
Nick Pettazzoni <pettazz@gmail.com>
2013-12-24 02:08:55 -0500

    make invitations admin only

--- public/include/pages/account/invitations.inc.php
+++ public/include/pages/account/invitations.inc.php
@@ -4,21 +4,26 @@
 if (!defined('SECURITY')) die('Hacking attempt');
 
 if ($user->isAuthenticated()) {
-  if (!$setting->getValue('disable_invitations')) {
-    if ($invitation->getCountInvitations($_SESSION['USERDATA']['id']) >= $config['accounts']['invitations']['count']) {
-      $_SESSION['POPUP'][] = array('CONTENT' => 'You have exceeded the allowed invitations of ' . $config['accounts']['invitations']['count'], 'TYPE' => 'errormsg');
-    } else if (isset($_POST['do']) && $_POST['do'] == 'sendInvitation') {
-      if ($invitation->sendInvitation($_SESSION['USERDATA']['id'], $_POST['data'])) {
-        $_SESSION['POPUP'][] = array('CONTENT' => 'Invitation sent');
-      } else {
-        $_SESSION['POPUP'][] = array('CONTENT' => 'Unable to send invitation to recipient: ' . $invitation->getError(), 'TYPE' => 'errormsg');
+  if($user->isAdmin($_SESSION['USERDATA']['id'])){
+    if (!$setting->getValue('disable_invitations')) {
+      if ($invitation->getCountInvitations($_SESSION['USERDATA']['id']) >= $config['accounts']['invitations']['count']) {
+        $_SESSION['POPUP'][] = array('CONTENT' => 'You have exceeded the allowed invitations of ' . $config['accounts']['invitations']['count'], 'TYPE' => 'errormsg');
+      } else if (isset($_POST['do']) && $_POST['do'] == 'sendInvitation') {
+        if ($invitation->sendInvitation($_SESSION['USERDATA']['id'], $_POST['data'])) {
+          $_SESSION['POPUP'][] = array('CONTENT' => 'Invitation sent');
+        } else {
+          $_SESSION['POPUP'][] = array('CONTENT' => 'Unable to send invitation to recipient: ' . $invitation->getError(), 'TYPE' => 'errormsg');
+        }
       }
+      $aInvitations = $invitation->getInvitations($_SESSION['USERDATA']['id']);
+      $smarty->assign('INVITATIONS', $aInvitations);
+    } else {
+      $aInvitations = array();
+      $_SESSION['POPUP'][] = array('CONTENT' => 'Invitations are disabled', 'TYPE' => 'errormsg');
     }
-    $aInvitations = $invitation->getInvitations($_SESSION['USERDATA']['id']);
-    $smarty->assign('INVITATIONS', $aInvitations);
   } else {
     $aInvitations = array();
-    $_SESSION['POPUP'][] = array('CONTENT' => 'Invitations are disabled', 'TYPE' => 'errormsg');
+    $_SESSION['POPUP'][] = array('CONTENT' => 'Invitations have been disabled', 'TYPE' => 'errormsg');
   }
 }
 $smarty->assign('CONTENT', 'default.tpl');
